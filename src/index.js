fab319b2341dd233edf275361a7be837ff4c99e8
Lorel <lorel.oboue@gmail.com>
2022-06-27 18:11:33 +0100

    variabled renamed and final touches added

--- src/index.js
+++ src/index.js
@@ -3,67 +3,67 @@ for (let i = 0; i < data.length; i++) {
   const pokemon = data[i];
 
   const card = document.createElement("li");
+  card.style.listStyle = "none";
   card.setAttribute("class", "card");
   cards.append(card);
 
-  const bulbasaurTitle = document.createElement("h2");
-  bulbasaurTitle.setAttribute("class", "card--title");
-  bulbasaurTitle.innerText = pokemon.name;
-  card.append(bulbasaurTitle);
+  const cardTitle = document.createElement("h2");
+  cardTitle.setAttribute("class", "card--title");
+  cardTitle.innerText = pokemon.name;
+  cardTitle.style.textTransform = "capitalize";
+  card.append(cardTitle);
 
-  const bulbasaurImg = document.createElement("img");
-  bulbasaurImg.setAttribute("class", "card--img");
-  bulbasaurImg.width = "256";
-  card.append(bulbasaurImg);
-  bulbasaurImg.src = pokemon.sprites.other["official-artwork"].front_default;
-  //   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png";
+  const pokemonImg = document.createElement("img");
+  pokemonImg.setAttribute("class", "card--img");
+  pokemonImg.width = "256";
+  card.append(pokemonImg);
+  pokemonImg.src = pokemon.sprites.other["official-artwork"].front_default;
 
-  const bulbasaurStats = document.createElement("ul");
-  bulbasaurStats.setAttribute("class", "card--text");
+  const pokemonStats = document.createElement("ul");
+  pokemonStats.setAttribute("class", "card--text");
+  card.append(pokemonStats);
 
   const hpLi = document.createElement("li");
   hpLi.innerText =
     pokemon.stats[0].stat.name.toUpperCase() +
     ": " +
     pokemon.stats[0].base_stat;
-  bulbasaurStats.append(hpLi);
+  pokemonStats.append(hpLi);
 
   const attackLi = document.createElement("li");
   attackLi.innerText =
     pokemon.stats[1].stat.name.toUpperCase() +
     ": " +
     pokemon.stats[1].base_stat;
-  bulbasaurStats.append(attackLi);
+  pokemonStats.append(attackLi);
 
   const defenseLi = document.createElement("li");
   defenseLi.innerText =
     pokemon.stats[2].stat.name.toUpperCase() +
     ": " +
     pokemon.stats[2].base_stat;
-  bulbasaurStats.append(defenseLi);
+  pokemonStats.append(defenseLi);
 
   const specialAttackLi = document.createElement("li");
   specialAttackLi.innerText =
     pokemon.stats[3].stat.name.toUpperCase() +
     ": " +
     pokemon.stats[3].base_stat;
-  bulbasaurStats.append(specialAttackLi);
+  pokemonStats.append(specialAttackLi);
 
   const specialDefenseLi = document.createElement("li");
   specialDefenseLi.innerText =
     pokemon.stats[4].stat.name.toUpperCase() +
     ": " +
     pokemon.stats[4].base_stat;
-  bulbasaurStats.append(specialDefenseLi);
+  pokemonStats.append(specialDefenseLi);
 
   const speedLi = document.createElement("li");
   speedLi.innerText =
     pokemon.stats[5].stat.name.toUpperCase() +
     ": " +
     pokemon.stats[5].base_stat;
-  bulbasaurStats.append(speedLi);
-
-  card.append(bulbasaurStats);
+  pokemonStats.append(speedLi);
 }
 
 //You can start simple and just render a single
